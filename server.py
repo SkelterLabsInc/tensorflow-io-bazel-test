8cd9ffdb1a49e96c9da816a1a79e5fcb5ee28efa
HyunSeok Kil <hyunseok@wisefour.com>
2021-09-16 12:03:06 +0900

    Update example code to share tensorflow-io bazel rules_python problem.

--- server.py
+++ server.py
@@ -3,4 +3,8 @@ import tensorflow as tf
 import tensorflow_io as tfio
 
 if __name__ == '__main__':
-    print('Hello world')
+    # Succeeded when we install tensorflow_io by using pip.
+    # Failed when we use bazel, bacause 
+    # https://github.com/bazelbuild/rules_python/blob/main/python/pip_install/extract_wheels/lib/purelib.py#L49
+    # skip to extract purelib to target directory.
+    print(tfio.__version__)
